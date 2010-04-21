9dfbc5fce81cc0370d84016354ddc6879e5440e7
Zhorken <zhorken@gmail.com>
2010-04-21 14:52:09 -0400

    Added RS Pokémon flavour ripping.

---
+++ rs_pokemon.py
@@ -0,0 +1,39 @@
+from sys import argv
+
+version = argv[2].lower()
+if version in ('ruby', 'r'):
+    offset = 0x3a0675
+elif version in('sapphire', 's'):
+    offset = 0x3a04bd
+
+table = {}
+for line in open('rs.tbl'):
+    line = line.rstrip('\n')
+    byte, char = line.split('=', 1)
+
+    byte = int(byte, 16)
+    if char == '\\n':
+        char = '\n'
+
+    table[byte] = char
+
+pokemon_names = open('pokemon.txt')
+game = open(argv[1], 'rb')
+game.seek(offset)
+
+for pokemon in range(386):
+    name = pokemon_names.readline().strip()
+    print(name)            # Bulbasaur
+    print('=' * len(name)) # =========
+
+    for page in (1, 2):
+        while True:
+            byte = game.read(1)
+            if byte == b'\xff':
+                break
+ 
+            print(table.setdefault(
+                ord(byte), '\\x{0:02x}'.format(ord(byte))
+            ), end='')
+
+        print('\n\n', end='')
